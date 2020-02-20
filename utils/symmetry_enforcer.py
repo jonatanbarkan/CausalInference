48dc5de9af123169dd96f69b8395fd8db083c8bf
jonatanbarkan <jbarkan14@gmail.com>
2020-02-20 15:15:53 +0200

    Typing example in symmetry_enforcer

--- utils/symmetry_enforcer.py
+++ utils/symmetry_enforcer.py
@@ -1,7 +1,8 @@
 import torch as th
+from typing import Union, List
 
 
-def th_enforce_symmetry(batch, label, anti=True):
+def th_enforce_symmetry(batch: List[th.Tensor], label: th.Tensor, anti=True):
     anti_batch = [b.flip(1) for b in batch]
     new_label = 1 - label if anti else label
     return anti_batch, new_label
